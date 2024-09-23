a75f616e19dfae7e1c4c2beb813ba9f369d5105e
Elkadhi Oussama <83676401+elkadhi-oussama@users.noreply.github.com>
2024-09-23 21:44:05 +0100

    update

--- Todo/userRouter.js
+++ Todo/userRouter.js
@@ -19,7 +19,7 @@ router.post('/register', async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({ username, password: hashedPassword });
     await newUser.save();
-    res.status(201).json({ message: 'User created successfully' });
+    res.status(201).json({ message: 'User created successfully', user : newUser });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
@@ -36,7 +36,7 @@ router.post('/login', async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
-    res.status(200).json({ message: 'Login successful' });
+    res.status(200).json({ message: 'Login successful', user : newUser });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
